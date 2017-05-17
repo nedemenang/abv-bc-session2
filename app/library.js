2e20cf526d61b0dbf970bc920f5507b4d84bd546
ABS User <ABS User>
2017-05-17 13:21:47 +0100

    Create car class

--- app/library.js
+++ app/library.js
@@ -1,3 +1,4 @@
+'use strict';
 
 module.exports = {
 
@@ -18,6 +19,45 @@ module.exports = {
 					}
 			}
 		return output;	
+	},
+
+	Car: (name, model, type) =>
+	{
+	   this.name = name;
+	   this.model = model;
+	   this.numOfWheels = 4;
+	   this.isSaloon = true;
+	   this.speed = '0 km/h';
+	   this.numberOfDoors = 4;
+	   
+	   if(name === "")
+	   {
+	     this.name = 'General';
+	     this.model = 'GM';
+	   }
+	   if (name === "Porshe" || name ==="Koenigsegg")
+	   {
+	     this.numberOfDoors = 2;
+	   }
+	   
+	   if(type === "trailer")
+	   {
+	     this.numOfWheels = 8;
+	     this.isSaloon = false;
+	   }
+	},
+
+	drive : function(gear) 
+	{
+	     if(this.type === "trailer")
+	     {
+	       let s = gear * 7;
+	       this.speed = s + " km/h" ;
+	     }
+	     else
+	     {
+	       let s = gear * 50;
+	       this.speed = s + " km/h" ;
+	     }
 	}
 }
-
