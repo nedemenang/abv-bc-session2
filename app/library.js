d504a06c661afffac1160778f57f81e188d32c61
ABS User <ABS User>
2017-05-17 10:29:56 +0100

    Write fizzBuzz function, Pass all test

--- app/library.js
+++ app/library.js
@@ -1,64 +1,23 @@
 
 module.exports = {
-//computeAverage: (num1, num2, num3) => {
 
-	aritGeo: (array) =>
+	fizzBuzz: (number) =>
 	{
-
-		if(array.length < 1) return 0;
-
-		let difference = array[1] - array[0];
-		let ratio = array[1] / array[0];
-
-		let isArithmetic = true;
-		let isGeometric = true;
-
-		for(let i = 0; i < array.length - 1; i++)
-		{
-		    if( array[i + 1] - array[i] !== difference )
-		      isArithmetic = false;
-		    if(array[i + 1] / ratio !== array[i])
-		      isGeometric = false;
-		}
-
-		if(isArithmetic === true)
-		    return "Arithmetic";
-		else if(isGeometric === true)
-		    return"Geometric";
-		else
-		    return -1;
-	},
-
-	findMinMax: (array) =>
-	{
-		if(array.length < 1) return 0;
-
-		  let minValue = array[0];
-		  let maxValue = array[0];
-		  let outputArray = [];
-		  for (let i = 0; i < array.length; i++) {
-		        if (array[i] < minValue) 
-		        {
-		            minValue = array[i];
-		        }
-		        
-		        if(array[i] > maxValue)
-		        {
-		          maxValue = array[i];
-		        }
-		    }
-		    
-		    if(minValue === maxValue)
-		    {
-		      outputArray.push(minValue);
-		    }
-		    else
-		    {
-		      
-		      outputArray.push(minValue);
-		      outputArray.push(maxValue);
-		    }
-		    return outputArray;
+		let output = number;
+		
+		if (number % 3 === 0 || number % 5 === 0) 
+			{
+				output = "";
+				if (number % 3 === 0) 
+					{
+						output = output + "Fizz";
+					}
+				if (number % 5 === 0) 
+					{
+						output = output + "Buzz";
+					}
+			}
+		return output;	
 	}
 }
 
